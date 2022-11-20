4fc67b1cd4059dd5f33865cc0785de00eb7593a3
Tomasz Szuster <tomasz.szuster@gmail.com>
2022-11-20 14:04:28 +0100

    :white_check_mark: Adding tests to the ALB construct

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="cdk-opinionated-constructs",
-    version="1.6.0",
+    version="1.7.1",
     description="AWS CDK constructs come without added security configurations.",
     long_description="Very rarely this is validated during the CI pipeline via tools like CDK-NAG. The idea behind this project is to create secured constructs from the start.",
     license="MIT",
     package_dir={"": "."},
     packages=find_packages(where="."),
     install_requires=[
-        "aws-cdk-lib>=2.50.0",
+        "aws-cdk-lib>=2.51.1",
         "constructs>=10.0.0,<11.0.0",
     ],
     python_requires=">=3.9",
