0937d571b643464863eb7cb633ba438fcfc1a1da
Tomasz Szuster <tomasz.szuster@gmail.com>
2023-01-27 09:26:51 +0100

    :arrow_up: Upgrading dependencies.

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(
     package_dir={"": "."},
     packages=find_packages(where="."),
     install_requires=[
-        "aws-cdk-lib>=2.51.1",
-        "constructs>=10.0.0,<11.0.0",
-        "cdk-monitoring-constructs>=1.28.0,<3.0.0",
+        "aws-cdk-lib>=2.62.1",
+        "constructs>=10.1.232,<11.0.0",
+        "cdk-monitoring-constructs>=2.3.0,<3.0.0",
     ],
     python_requires=">=3.9",
 )
